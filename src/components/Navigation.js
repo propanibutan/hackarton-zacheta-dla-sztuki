dee1279a797a19661395b4526aa9b2afbd719a7b
Patrycja Zalas <minekosakura@gmail.com>
2022-09-24 12:45:58 +0200

    new

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -8,7 +8,7 @@ export default function Navigation() {
           <svg className="magnifier" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352c79.5 0 144-64.5 144-144s-64.5-144-144-144S64 128.5 64 208s64.5 144 144 144z"/></svg>
           <input className="input-social" type="text" id="search" name="search" aria-label="Szukaj"></input>
         </div>
-            <a className="icons-social" aria-label="link do konta strony na tictocu" href="https://www.tictoc.com">
+            <a className="icons-social" aria-label="link do konta strony na tiktoku" href="https://www.tiktok.com">
                 <svg width="20px" height="20px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M448,209.91a210.06,210.06,0,0,1-122.77-39.25V349.38A162.55,162.55,0,1,1,185,188.31V278.2a74.62,74.62,0,1,0,52.23,71.18V0l88,0a121.18,121.18,0,0,0,1.86,22.17h0A122.18,122.18,0,0,0,381,102.39a121.43,121.43,0,0,0,67,20.14Z"/></svg>
             </a>
             <a className="icons-social" aria-label="link do konta strony na instagramie" href="https://www.instagram.com">
