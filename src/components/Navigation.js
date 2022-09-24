432b1a3f280c4813273702c93e881fdb814db638
Patrycja Zalas <minekosakura@gmail.com>
2022-09-24 11:48:23 +0200

    new

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -28,19 +28,20 @@ export default function Navigation() {
                     <a href="#kolekcje" aria-label="link do podstrony 'kolekcje'">KOLEKCJE</a>
                 </li>
             </ul>
-            <div className="navigation-end">
-              
-              <button className="icon-Aa" aria-label="zmiana wielkości fontów na stronie">
-                <svg width="16px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M253.5 51.7C248.6 39.8 236.9 32 224 32s-24.6 7.8-29.5 19.7l-120 288-40 96c-6.8 16.3 .9 35 17.2 41.8s35-.9 41.8-17.2L125.3 384H322.7l31.8 76.3c6.8 16.3 25.5 24 41.8 17.2s24-25.5 17.2-41.8l-40-96-120-288zM296 320H152l72-172.8L296 320z"/></svg>
-                <svg width="24px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M253.5 51.7C248.6 39.8 236.9 32 224 32s-24.6 7.8-29.5 19.7l-120 288-40 96c-6.8 16.3 .9 35 17.2 41.8s35-.9 41.8-17.2L125.3 384H322.7l31.8 76.3c6.8 16.3 25.5 24 41.8 17.2s24-25.5 17.2-41.8l-40-96-120-288zM296 320H152l72-172.8L296 320z"/></svg>
-              </button>
-            
-            <select id="language" name="language" className="language-select" aria-label="wybór języka strony">
-                <option>PL</option>
-                <option>EN</option>
-            </select>
-            <button type="button" aria-label="przycisk zaloguj" className="login-button">Zaloguj/Zarejestruj</button>
-            </div>
+      <div className="navigation-end">
+        
+        <button className="icon-Aa" aria-label="zmiana wielkości fontów na stronie">
+          <svg width="16px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M253.5 51.7C248.6 39.8 236.9 32 224 32s-24.6 7.8-29.5 19.7l-120 288-40 96c-6.8 16.3 .9 35 17.2 41.8s35-.9 41.8-17.2L125.3 384H322.7l31.8 76.3c6.8 16.3 25.5 24 41.8 17.2s24-25.5 17.2-41.8l-40-96-120-288zM296 320H152l72-172.8L296 320z"/></svg>
+          <svg width="24px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M253.5 51.7C248.6 39.8 236.9 32 224 32s-24.6 7.8-29.5 19.7l-120 288-40 96c-6.8 16.3 .9 35 17.2 41.8s35-.9 41.8-17.2L125.3 384H322.7l31.8 76.3c6.8 16.3 25.5 24 41.8 17.2s24-25.5 17.2-41.8l-40-96-120-288zM296 320H152l72-172.8L296 320z"/></svg>
+        </button>
+      
+      <select id="language" name="language" className="language-select" aria-label="wybór języka strony">
+          <option>PL</option>
+          <option>EN</option>
+      </select>
+      <button type="button" aria-label="przycisk zaloguj" className="login-button btn-small">Zaloguj/Zarejestruj</button>
+
+      </div>
       </nav>
     </div>
   )
